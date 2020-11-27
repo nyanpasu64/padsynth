e0dafdd76ea7877f53e3a96240a8264d17b4fb2c
nyanpasu64 <nyanpasu64@tuta.io>
2020-11-27 10:54:37 -0800

    Add CLI arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+/// TODO fill this out
+#[derive(StructOpt, Debug)]
+#[structopt(name = "padsynth")]
+pub struct Opt {
+    /// Audio file to resynthesize
+    #[structopt(name = "WAV", parse(from_os_str))]
+    wav: PathBuf,
+
+    /// Configuration file for resynthesis
+    #[structopt(name = "CONFIG", parse(from_os_str))]
+    cfg: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opt = Opt::from_args();
+
+    println!("{:?}", opt);
 }
