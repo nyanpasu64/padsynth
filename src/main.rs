02b22d420584452a1e1a88b556be20a1a45901db
nyanpasu64 <nyanpasu64@tuta.io>
2020-11-27 12:26:09 -0800

    Switch to wav crate

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use anyhow::{Context, Result};
-use std::{fs::File, io::BufReader, path::PathBuf};
+use std::fs::File;
+use std::io::BufReader;
+use std::path::PathBuf;
 use structopt::StructOpt;
 
 /// TODO fill this out
@@ -15,47 +17,21 @@ pub struct Opt {
     cfg: PathBuf,
 }
 
-type WavReader = hound::WavReader<BufReader<File>>;
-
-struct WavToFloat {
-    wav: WavReader,
-}
-
-impl WavToFloat {
-    fn new(wav: WavReader) -> WavToFloat {
-        WavToFloat{wav}
-    }
-
-    fn read(&mut self) -> Result<Vec<f32>> {
-        let spec = self.wav.spec();
-
-        match (spec.sample_format, spec.bits_per_sample) {
-            (hound::SampleFormat::Float, 32) => {
-                Ok(self.wav.samples::<f32>().collect())
-            },
-            _ => anyhow::bail!(
-                "Unsupported sample format {:?}{}",
-                spec.sample_format,
-                spec.bits_per_sample,
-            ),
-        }
-    }
-}
-
 fn main() -> Result<()> {
     let opt = Opt::from_args();
 
-    let wav = WavReader::open(&opt.wav).with_context(|| {
+    let wav_error = || {
         format!(
             "cannot open WAV file '{}' specified in command line",
             opt.wav.display(),
         )
-    })?;
+    };
 
+    let file = File::open(&opt.wav).with_context(&wav_error)?;
+    let mut buf_reader = BufReader::new(file);
 
-    let _wav_data = ;
-
-    println!("{:?}", opt);
+    let _wav_data = wav::read(&mut buf_reader).with_context(&wav_error)?;
+    // println!("{:?}", wav_data);
 
     Ok(())
 }
