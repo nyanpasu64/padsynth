6eaa3b48029b552795a1d3ca4eaa02facf09d46c
nyanpasu64 <nyanpasu64@tuta.io>
2020-11-29 01:11:02 -0800

    Move `mod dsp` to new file

---
+++ src/dsp.rs
@@ -0,0 +1,41 @@
+use crate::cfg::Config;
+use crate::{cfg::Pitch, common::Amplitude};
+
+fn cents_to_freq_mul(cents: f32) -> f32 {
+    2.0f32.powf(cents / 1200.0)
+}
+
+fn midi_to_freq(midi: i32) -> f32 {
+    440.0 * 2.0f32.powf((midi - 69) as f32 / 12.0)
+}
+
+fn pitch_to_freq(pitch: Pitch) -> f32 {
+    match pitch {
+        Pitch::Hz(f) => f,
+        Pitch::Midi(midi) => midi_to_freq(midi),
+    }
+}
+
+/// `cfg` represents the padsynth setup, read from a config file.
+///
+/// `data` represents the entire wav file, downmixed to mono,
+/// but not yet trimmed to the looped section only.
+///
+pub fn process(cfg: &Config, mut data: &[Amplitude], orig_sample_rate: u32) -> Vec<Amplitude> {
+    let input = &cfg.input;
+
+    // Trim the wav data to the looped portion.
+    let loop_begin = input.loop_begin;
+    let loop_end = input.loop_end.unwrap_or(data.len());
+    data = &data[loop_begin..loop_end];
+
+    let mut sample_rate = input.transpose.sample_rate.unwrap_or(orig_sample_rate) as f32;
+    sample_rate *= cents_to_freq_mul(input.transpose.detune_cents);
+    let pitch = pitch_to_freq(input.pitch);
+
+    // Take the FFT of the looped portion.
+    // We cannot use realfft, because it assumes the input has even length
+    // (which may not be true for arbitrary looped samples).
+    // It is true for samples ripped from SNES games (multiple of 16).
+    unimplemented!()
+}
