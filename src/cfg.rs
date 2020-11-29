d4d050aa7b9f6766a696721aa1b0904ce212b352
nyanpasu64 <nyanpasu64@tuta.io>
2020-11-28 22:13:48 -0800

    Mark cfg struct fields as pub

--- src/cfg.rs
+++ src/cfg.rs
@@ -3,33 +3,33 @@ use serde::Deserialize;
 /// File config struct.
 #[derive(Deserialize, Debug)]
 pub struct Config {
-    input: Input,
-    output: Output,
+    pub input: Input,
+    pub output: Output,
 }
 
 #[derive(Deserialize, Debug)]
 pub struct Input {
     /// TODO If omitted, smpl chunk must be present, and will be used for loop begin.
-    loop_begin: u32,
+    pub loop_begin: u32,
 
     /// Not included in loop.
     /// If omitted, defaults to smpl chunk if present, otherwise end of sample.
     #[serde(default)]
-    loop_end: Option<u32>,
+    pub loop_end: Option<u32>,
 
-    transpose: Transpose,
+    pub transpose: Transpose,
 
     /// Used to split the input signal up into bins.
-    pitch: Pitch,
+    pub pitch: Pitch,
 }
 
 #[derive(Deserialize, Debug, Default)]
 pub struct Transpose {
     #[serde(default)]
-    sample_rate: Option<u32>,
+    pub sample_rate: Option<u32>,
 
     #[serde(default)]
-    detune_cents: f32,
+    pub detune_cents: f32,
 }
 
 #[derive(Deserialize, Debug)]
@@ -40,15 +40,15 @@ pub enum Pitch {
 
 #[derive(Deserialize, Debug)]
 pub struct Output {
-    sample_rate: u32,
-    duration: Duration,
+    pub sample_rate: u32,
+    pub duration: Duration,
     // TODO mode: PreserveSpectrum | Harmonic{stdev} | PreserveFormants{stdev, fund_pitch}
     // and remove harmonic_stdev
-    harmonic_stdev: f32,
+    pub harmonic_stdev: f32,
 
     #[serde(default)]
-    master_volume: Volume,
-    chord: Vec<ChordNote>,
+    pub master_volume: Volume,
+    pub chord: Vec<ChordNote>,
 }
 
 #[derive(Deserialize, Debug)]
@@ -59,8 +59,8 @@ pub enum Duration {
 
 #[derive(Deserialize, Debug)]
 pub struct ChordNote {
-    pitch: ChordPitch,
-    volume: Volume,
+    pub pitch: ChordPitch,
+    pub volume: Volume,
 }
 
 #[derive(Deserialize, Debug)]
